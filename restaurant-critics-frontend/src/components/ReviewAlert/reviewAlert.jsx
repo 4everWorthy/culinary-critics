fe54b652add53358837ddd8eaee479ae5173ada5
CarrieCanDo <carrie.a.wright@outlook.com>
2024-10-30 16:06:18 -0400

    fixed review alert alignment

--- restaurant-critics-frontend/src/components/ReviewAlert/reviewAlert.jsx
+++ restaurant-critics-frontend/src/components/ReviewAlert/reviewAlert.jsx
@@ -4,9 +4,11 @@ import React from 'react';
 import './reviewAlert.css';
 
 const ReviewAlert = ({ user, reviewDate }) => {
+    const date = new Date(reviewDate);
+    const formattedDate = new Date(date).toLocaleDateString();
     return (
         <div className="review-alert">
-            <p>{user || 'Anonymous'} wrote a review. {reviewDate}</p>
+            <p>{user || 'Anonymous'} wrote a review. {formattedDate}</p>
         </div>
     );
 };
